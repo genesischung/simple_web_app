2e683faab1c392bb45fe7bb9141b555df9fa0576
Genesis Zhong <genesischung@me.com>
2023-08-18 22:07:52 +0800

    Fixed bug in index.html, added healthcheck

--- app/f1.py
+++ app/f1.py
@@ -3,18 +3,27 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
+
+
 from app.auth import login_required
 from app.db import get_db
 
 bp = Blueprint('f1', __name__)
+
 
 @bp.route('/')
 def index():
     db = get_db()
     return render_template('index.html', text="Hello World!")
 
+
 @bp.route("/message", methods=["POST"])
 @login_required
 def echo_input():
     input_text = request.form.get("user_input", "")
     return render_template('index.html', text=input_text)
+
+@bp.route("/health")
+def health_check():
+    return render_template('health_check.html'), 200
+
